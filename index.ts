0a986b00919dfa2fd20503c075834cc2a093b1f8
Flavia Nunes <flavianunesbarboza@gmail.com>
2025-03-24 09:54:07 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,39 +1,57 @@
-class Autor{
-    nome: string 
-    nacionalidade: string 
-    dt_nascimento: string 
+class Autor {
+    private nome: string;
+    private nacionalidade: string;
+    private dt_nascimento: string;
 
-    constructor(nome:string, nacionalidade:string, dt_nascimento:string){
+    constructor(nome: string, nacionalidade: string, dt_nascimento: string) {
         this.nome = nome;
         this.nacionalidade = nacionalidade;
-        this.dt_nascimento = dt_nascimento
+        this.dt_nascimento = dt_nascimento;
     }
 
+    get info(): string {
+        return `>>>>> AUTOR <<<<< \nNome: ${this.nome} \nNacionalidade: ${this.nacionalidade} \nNascimento: ${this.dt_nascimento} \n`;
+    }
 }
 
-class Livro{
-    titulo: string 
-    ano_publicacao: number 
+class Livro {
+    private titulo: string;
+    private ano_publicacao: number;
+    private autor: Autor;
 
-    constructor(titulo:string, ano_publicacao:number){
+    constructor(titulo: string, ano_publicacao: number, autor: Autor) {
         this.titulo = titulo;
         this.ano_publicacao = ano_publicacao;
+        this.autor = autor;
+    }
+
+    get info(): string {
+        return `>>>>>LIVRO <<<<< \nTítulo: ${this.titulo} \nAno de Publicação: ${this.ano_publicacao} \n${this.autor.info} \n`;
     }
 }
 
-class Usuario{
-    nome: string 
-    idade: number
+class Usuario {
+    private nome: string;
+    private idade: number;
+    private livro: Livro;
 
-    constructor(nome:string, idade:number){
-        this.nome = nome
-        this.idade = idade
+    constructor(nome: string, idade: number, livro: Livro) {
+        this.nome = nome;
+        this.idade = idade;
+        this.livro = livro;
+    }
+
+    get info(): string {
+        return `>>>>>USUÁRIO <<<<< \nNome: ${this.nome} \nIdade: ${this.idade} \n${this.livro.info} \n`;
     }
 }
 
-const autor = new Autor("Clarice Lispector", "Portuguesa", "01/02/1998")
-console.log(autor)
-const livro = new Livro("Hora da estrela", 1998)
-console.log(livro)
-const usuario = new Usuario("Flavia", 18)
-console.log(usuario)
+// Criando instâncias corretamente
+const autor = new Autor("Clarice Lispector", "Brasileira", "10/12/1920");
+console.log(autor.info);
+
+const livro = new Livro("A Hora da Estrela", 1977, autor);
+console.log(livro.info);
+
+const usuario = new Usuario("Flávia", 18, livro);
+console.log(usuario.info);
